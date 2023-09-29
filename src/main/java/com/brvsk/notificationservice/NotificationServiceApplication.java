05f7fd8f7017ea7d5e76ee405b9f34742bcdac15
Kacper <kacpersjuszb@gmail.com>
2023-09-29 14:40:31 +0200

    first commit

---
+++ src/main/java/com/brvsk/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,26 @@
+package com.brvsk.notificationservice;
+
+import com.brvsk.commons.event.OrderPlacedEvent;
+import io.micrometer.observation.Observation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message <{}>", orderPlacedEvent);
+    }
+
+}
